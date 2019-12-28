af305a420f2447896b43bf368ca48af9becfbd00
kasperg3 <kasperg3@rocketmail.com>
2019-12-28 23:13:26 +0100

    Added marlin and moved config files to marlin folder

--- GoProCapture.py
+++ GoProCapture.py
@@ -8,17 +8,18 @@ from goprocam import constants
 
 
 gpCam = GoProCamera.GoPro()
-gpCam.mode(constants.Mode.PhotoMode, constants.Mode.SubMode.Photo.Single_H5)
+gpCam.mode(constants.Mode.PhotoMode, constants.Mode.SubMode.Photo.Single)
 
 url = gpCam.take_photo()
 print(url)
 resp = requests.get(url, stream=True).raw
 image = np.asarray(bytearray(resp.read()), dtype="uint8")
 image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+gpCam.delete("all")
 
 # for testing
-cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-cv2.resizeWindow('image', 600,600)
-cv2.imshow('image',image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+# cv2.resizeWindow('image', 600,600)
+# cv2.imshow('image',image)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
